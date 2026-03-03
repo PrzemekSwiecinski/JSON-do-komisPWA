5b423db352454dc9c8e7b02eda1fa6594739f9f5
Przemek <100935707+PrzemekSwiecinski@users.noreply.github.com>
2026-03-03 19:28:28 +0100

    Add files via upload

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="navbar-logo">
+          Komis PWA
+        </div>
+        
+        <div className="navbar-links">
+          <button>O Nas</button>
+          <button>Kontakt</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
